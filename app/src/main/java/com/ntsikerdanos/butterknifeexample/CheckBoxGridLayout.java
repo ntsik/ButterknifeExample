16516c2a0a6215af6a80938d0f9f87b389b5c223
Nick Tsikerdanos <ntsikerdanos@mobiquityinc.com>
2015-03-04 12:59:00 -0600

    Update to 2.0
    Refactor CheckBoxGridLayout into its own class with all associated
    functionality.
    
    Now requires API 17+ due to View.generateViewId()

---
+++ app/src/main/java/com/ntsikerdanos/butterknifeexample/CheckBoxGridLayout.java
@@ -0,0 +1,153 @@
+package com.ntsikerdanos.butterknifeexample;
+
+import android.content.Context;
+import android.support.v7.widget.GridLayout;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.CheckBox;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CheckBoxGridLayout extends GridLayout {
+	// TODO: Make this configurable in XML and code
+	private static final int WIDTH = 3;
+	private static final int HEIGHT = 3;
+
+	// TODO: This should be in a style
+	private static final float CHECKBOX_SCALE = 1.5f;
+
+	private List<CheckBox> checkBoxes = Collections.emptyList();
+	private OnChangedListener listener;
+
+	public CheckBoxGridLayout(Context context) {
+		this(context, null);
+	}
+
+	public CheckBoxGridLayout(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	public CheckBoxGridLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+
+		setOrientation(HORIZONTAL);
+
+		fillGrid();
+
+		setColumnCount(WIDTH);
+		setRowCount(HEIGHT);
+	}
+
+	public int getSize() {
+		return checkBoxes.size();
+	}
+
+	public int getUncheckedCount() {
+		int unchecked = 0;
+
+		for (CheckBox checkBox : checkBoxes) {
+			if (!checkBox.isChecked()) {
+				++unchecked;
+			}
+		}
+
+		return unchecked;
+	}
+
+	public void reset() {
+		for (CheckBox checkBox : checkBoxes) {
+			checkBox.setChecked(false);
+			checkBox.setEnabled(true);
+		}
+	}
+
+	public void setOnChangedListener(OnChangedListener listener) {
+		this.listener = listener;
+	}
+
+	private void fillGrid() {
+		for (CheckBox checkBox : checkBoxes) {
+			removeView(checkBox);
+		}
+
+		checkBoxes = generateCheckBoxes();
+
+		for (int i = 0; i < checkBoxes.size(); i++) {
+			CheckBox checkBox = checkBoxes.get(i);
+
+			if (i + WIDTH < checkBoxes.size()) {
+				checkBox.setNextFocusDownId(checkBoxes.get(i + WIDTH).getId());
+			}
+
+			if (i % WIDTH != 0 && i - 1 >= 0) {
+				checkBox.setNextFocusLeftId(checkBoxes.get(i - 1).getId());
+			}
+
+			if (i % WIDTH != WIDTH - 1 && i + 1 < checkBoxes.size()) {
+				checkBox.setNextFocusRightId(checkBoxes.get(i + 1).getId());
+			}
+
+			if (i - WIDTH >= 0) {
+				checkBox.setNextFocusUpId(checkBoxes.get(i - WIDTH).getId());
+			}
+
+			checkBox.setOnClickListener(checkBoxClickListener);
+
+			addView(checkBox);
+		}
+	}
+
+	private List<CheckBox> generateCheckBoxes() {
+		int size = WIDTH * HEIGHT;
+
+		List<CheckBox> checkBoxes = new ArrayList<>(size);
+
+		for (int i = 0; i < size; i++) {
+			CheckBox checkBox = new CheckBox(getContext());
+			checkBox.setId(View.generateViewId());
+			checkBox.setScaleX(CHECKBOX_SCALE);
+			checkBox.setScaleY(CHECKBOX_SCALE);
+
+			checkBoxes.add(checkBox);
+		}
+
+		return checkBoxes;
+	}
+
+	private View.OnClickListener checkBoxClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			invertCheckBox(v.getNextFocusDownId());
+			invertCheckBox(v.getNextFocusLeftId());
+			invertCheckBox(v.getNextFocusRightId());
+			invertCheckBox(v.getNextFocusUpId());
+
+			int uncheckedCount = getUncheckedCount();
+
+			if (listener != null) {
+				listener.onChanged(uncheckedCount);
+			}
+
+			if (uncheckedCount == 0) {
+				for (CheckBox checkBox : checkBoxes) {
+					checkBox.setEnabled(false);
+				}
+			}
+		}
+
+		private void invertCheckBox(int viewId) {
+			if (viewId == View.NO_ID) {
+				return;
+			}
+
+			CheckBox box = (CheckBox) findViewById(viewId);
+			box.setChecked(!box.isChecked());
+		}
+	};
+
+	public interface OnChangedListener {
+		void onChanged(int uncheckedCount);
+	}
+}
--- app/src/main/java/com/ntsikerdanos/butterknifeexample/MainActivity.java
+++ app/src/main/java/com/ntsikerdanos/butterknifeexample/MainActivity.java
@@ -2,13 +2,12 @@ package com.ntsikerdanos.butterknifeexample;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayout;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	private GridLayout grid;
+	CheckBoxGridLayout grid;
+	TextView status;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,84 +15,41 @@ public class MainActivity extends Activity {
 
 		setContentView(R.layout.activity_main);
 
-		grid = (GridLayout) findViewById(R.id.grid);
-
-		int size = (int) Math.sqrt(grid.getChildCount());
-		grid.setColumnCount(size);
-		grid.setRowCount(size);
+		grid = (CheckBoxGridLayout) findViewById(R.id.grid);
+		status = (TextView) findViewById(R.id.status);
 
 		resetStatus();
-
-		for (int i = 0; i < grid.getChildCount(); i++) {
-			grid.getChildAt(i).setOnClickListener(new CheckBoxClickListener());
-		}
 
 		findViewById(R.id.reset).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				for (int i = 0; i < grid.getChildCount(); i++) {
-					CheckBox checkBox = (CheckBox) grid.getChildAt(i);
-					checkBox.setChecked(false);
-					checkBox.setEnabled(true);
+				reset();
+			}
+		});
 
-					resetStatus();
-				}
+		grid.setOnChangedListener(new CheckBoxGridLayout.OnChangedListener() {
+			@Override
+			public void onChanged(int uncheckedCount) {
+				updateStatus(uncheckedCount);
 			}
 		});
 	}
 
-	private int getUncheckedCount() {
-		int unchecked = 0;
+	public void reset() {
+		grid.reset();
 
-		for (int i = 0; i < grid.getChildCount(); i++) {
-			if (!((CheckBox) grid.getChildAt(i)).isChecked()) {
-				++unchecked;
-			}
-		}
-
-		return unchecked;
+		resetStatus();
 	}
 
 	private void resetStatus() {
-		updateStatus(grid.getChildCount());
+		updateStatus(grid.getSize());
 	}
 
 	private void updateStatus(int count) {
-		TextView status = (TextView) findViewById(R.id.status);
-
 		if (count > 0) {
 			status.setText(getString(R.string.more_to_go_count, count));
 		} else {
 			status.setText(R.string.completed);
-		}
-	}
-
-	private class CheckBoxClickListener implements View.OnClickListener {
-		@Override
-		public void onClick(View v) {
-			invertCheckBox(v.getNextFocusDownId());
-			invertCheckBox(v.getNextFocusLeftId());
-			invertCheckBox(v.getNextFocusRightId());
-			invertCheckBox(v.getNextFocusUpId());
-
-			int uncheckedCount = getUncheckedCount();
-
-			updateStatus(uncheckedCount);
-
-			if (uncheckedCount == 0) {
-				for (int i = 0; i < grid.getChildCount(); i++) {
-					grid.getChildAt(i).setEnabled(false);
-				}
-			}
-		}
-
-		private void invertCheckBox(int viewId) {
-			if (viewId == View.NO_ID) {
-				return;
-			}
-
-			CheckBox box = (CheckBox) findViewById(viewId);
-			box.setChecked(!box.isChecked());
 		}
 	}
 }
